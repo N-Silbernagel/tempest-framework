247f9d66178e632c13bafadeed0c93f2443430ee
N-Silbernagel <6422477+N-Silbernagel@users.noreply.github.com>
2024-03-04 20:56:48 +0100

    use class for caching routing regex

--- src/Discovery/RouteDiscovery.php
+++ src/Discovery/RouteDiscovery.php
@@ -14,7 +14,7 @@ use Tempest\Http\RouteConfig;
 final readonly class RouteDiscovery implements Discovery
 {
     private const CACHE_PATH = __DIR__ . '/route-discovery.cache.php';
-    private const CACHE_PATH_REGEX = __DIR__ . '/route-regex.cache.php';
+    private const CACHE_PATH_REGEX = __DIR__ . '/RouteCache.php';
 
     public function __construct(private RouteConfig $routeConfig)
     {
@@ -35,20 +35,34 @@ final readonly class RouteDiscovery implements Discovery
 
     public function hasCache(): bool
     {
-        return file_exists(self::CACHE_PATH) && file_exists(self::CACHE_PATH_REGEX);
+        return file_exists(self::CACHE_PATH) && class_exists(self::CACHE_PATH_REGEX);
     }
 
     public function storeCache(): void
     {
         file_put_contents(self::CACHE_PATH, serialize($this->routeConfig->routes));
-        file_put_contents(self::CACHE_PATH_REGEX, "<?php return " . var_export($this->routeConfig->regex, true) . ";");
+        $routeCache = <<<PHP
+<?php
+
+namespace Tempest\Discovery;
+
+class RouteCache 
+{
+    public function getRegex(): string
+    {
+        return "{$this->routeConfig->regex}";
+    }
+}
+PHP;
+
+        file_put_contents(self::CACHE_PATH_REGEX, $routeCache);
     }
 
     public function restoreCache(Container $container): void
     {
         $routes = unserialize(file_get_contents(self::CACHE_PATH));
         $this->routeConfig->routes = $routes;
-        $this->routeConfig->regex = require self::CACHE_PATH_REGEX;
+        $this->routeConfig->regex = (new \Tempest\Discovery\RouteCache())->getRegex();
     }
 
     public function destroyCache(): void
